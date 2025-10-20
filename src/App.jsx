fedcea5b24e28316af98cbc661195431481c3bb7
Kanami Anderson <kanamianderson@gmail.com>
2025-10-20 11:28:25 -0600

    Initial commit - Fun Facts About React

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+export default function App() {
+  return (
+    <div style={{ maxWidth: 720 }}>
+      <img
+      src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+      alt="React logo"
+      width="40"
+      />
+
+      <h1>Fun facts about React</h1>
+
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+
+    </div>
+  );
+}
