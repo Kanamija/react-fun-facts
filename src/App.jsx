241538897ba79ac53d6a49e793e32bdba3b0a7a8
Kanami Anderson <kanamianderson@gmail.com>
2025-10-23 14:12:59 -0600

    Starting from scratch so deleted all files except index.jsx, index.jsx, and App.jsx. From there we created a new componets folder and added two component files Main.jsx and Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import Page from "./Page.jsx";
+import Main from "./components/Main.jsx"
+import Navbar from "./components/Navbar.jsx"
 
 export default function App() {
   return (
-    <div style={{ maxWidth: 720 }}>
-      <img
-      src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-      alt="React logo"
-      width="40"
-      />
-
-      <h1>Fun facts about React</h1>
-
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-
-      <Page />
-    </div>
-  );
-}
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+} 
--- src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer style={{ marginTop: "1.25rem", fontSize: "0.9rem", opacity: 0.8 }}>
-          <small>  © 20xx Anderson development. All rights reserved. </small>
-       </footer>
-    )
-
-}
